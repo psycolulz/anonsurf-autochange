24a235f62bd34d8db44c58d50842c7b320c21075
psycolulz <112537894+psycolulz@users.noreply.github.com>
2022-10-17 10:26:07 -0400

    Add files via upload

---
+++ autochange.sh
@@ -0,0 +1,16 @@
+#!/user/bin/env bash
+
+while true
+do
+    echo
+    echo  "____  ____ ___  _ ____  ____  _     _     _     ____" 
+    echo "/  __\/ ___\\  \///   _\/  _ \/ \   / \ /\/ \   /_   \ "
+    echo "|  \/||    \ \  / |  /  | / \|| |   | | ||| |    /   / "
+    echo "|  __/\___ | / /  |  \_ | \_/|| |_/\| \_/|| |_/\/   /_ "
+    echo "\_/   \____//_/   \____/\____/\____/\____/\____/\____/ "
+    echo                                                  
+    echo
+    echo "Press Ctrl + C To Exit This Loop"
+    command anonsurf change
+    sleep 8
+done
